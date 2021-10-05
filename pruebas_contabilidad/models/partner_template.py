3cc5d4aee71da03d256ab9258a3f35fe68a50792
PabloMarcial99 <17031116@itcelata.edu.mx>
2021-10-05 13:07:03 -0500

    Corregido la vista

--- pruebas_contabilidad/models/partner_template.py
+++ pruebas_contabilidad/models/partner_template.py
@@ -3,6 +3,8 @@
 from odoo import api,fields,models
 
 class PartnerTemplate(models.Model):
+    _name = 'res.partner'
     _inherit = 'res.partner'
+
 
     nuevo = fields.Text(string='Nuevo campo')
