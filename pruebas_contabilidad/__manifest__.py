b67d5cbd7ea4b663b06a90fa4ebbdb42bd516bd9
PabloMarcial99 <17031116@itcelata.edu.mx>
2021-10-05 12:54:11 -0500

    Creacion del modelo heredado

---
+++ pruebas_contabilidad/__manifest__.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+{
+    'name':'Prueba contabilidad',
+    'summary':'Pruebas de modificaciones al modulo de contabilidad',
+    'description':'''
+    Se va a modificar el modelo de cliente
+    para evitar que se guarde sin telefono
+    ''',
+    'author':'Juan Pablo Marcial',
+    'website':'https://github.com/Marcial99',
+    'category':'Training',
+    'version':'0.1',
+    'depends':[
+        'accounting'
+    ],
+    'data':[
+        'views/partner_template.xml'
+    ],
+    'demo':[]
+}
---
+++ pruebas_contabilidad/models/partner_template.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+
+from odoo import api,fields,models
+
+class PartnerTemplate(models.Model):
+    _inherit = 'res.partner'
+
+    nuevo = fields.Text(string='Nuevo campo')
