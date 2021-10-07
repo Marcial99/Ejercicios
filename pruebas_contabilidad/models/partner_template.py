6b2137e738788bccba31eb12ef9c2b32ae42a640
PabloMarcial99 <17031116@itcelata.edu.mx>
2021-10-07 12:39:35 -0500

    Prueba de restriccion

--- pruebas_contabilidad/models/partner_template.py
+++ pruebas_contabilidad/models/partner_template.py
@@ -1,9 +1,16 @@
 #-*- coding: utf-8 -*-
 
 from odoo import api,fields,models
-
+from odoo.exceptions import UserError
 class PartnerTemplate(models.Model):
     _inherit = 'res.partner'
 
 
     nuevo = fields.Text(string='Nuevo campo')
+
+
+    @api.model
+    def create(self, vals):
+        if len(nuevo) <1:
+            raise UserError('El campo nuevo es obligatorio')
+        return super().create(vals)
